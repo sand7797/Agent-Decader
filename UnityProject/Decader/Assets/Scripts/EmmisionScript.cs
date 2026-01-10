8540c05a8e0015d0bef55ce585c3a3012ddf16dc
sand7797 <sander@elsker.vodka>
2026-01-10 15:00:43 +0100

    forgot last patch

--- UnityProject/Decader/Assets/Scripts/EmmisionScript.cs
+++ UnityProject/Decader/Assets/Scripts/EmmisionScript.cs
@@ -3,83 +3,83 @@ using System.Collections;
 
 public class EmissionScript : MonoBehaviour
 {
-    public Material material;
-    public float unitTime = 0.2f;
-    public float bloomMultiplier = 0.5f;
+  public Material material;
+  public float unitTime = 0.2f;
+  public float bloomMultiplier = 0.5f;
 
-    [TextArea]
-    public string morsePattern = "/-.- . ... -/... -.- .-. .. -. --./";
+  [TextArea]
+  public string morsePattern = "/-.- . ... -/... -.- .-. .. -. --./";
 
-    void Start()
+  void Start()
+  {
+    StartCoroutine(PlayMorse(morsePattern));
+  }
+
+  IEnumerator PlayMorse(string pattern)
+  {
+    while (true)
     {
-        StartCoroutine(PlayMorse(morsePattern));
+      foreach (char c in pattern)
+      {
+	switch (c)
+	{
+	  case '.':
+	    yield return Dot();
+	    break;
+
+	  case '-':
+	    yield return Dash();
+	    break;
+
+	  case ' ':
+	    yield return LetterGap();
+	    break;
+
+	  case '/':
+	    yield return WordGap();
+	    break;
+
+	  default:
+	    break;
+	}
+      }
     }
+  }
 
-    IEnumerator PlayMorse(string pattern)
-    {
-        while (true)
-        {
-            foreach (char c in pattern)
-            {
-                switch (c)
-                {
-                    case '.':
-                        yield return Dot();
-                        break;
+  IEnumerator Dot()
+  {
+    EmissionOn();
+    yield return new WaitForSeconds(unitTime);
+    EmissionOff();
+    yield return new WaitForSeconds(unitTime);
+  }
 
-                    case '-':
-                        yield return Dash();
-                        break;
+  IEnumerator Dash()
+  {
+    EmissionOn();
+    yield return new WaitForSeconds(unitTime * 3f);
+    EmissionOff();
+    yield return new WaitForSeconds(unitTime);
+  }
 
-                    case ' ':
-                        yield return LetterGap();
-                        break;
+  IEnumerator LetterGap()
+  {
+    yield return new WaitForSeconds(unitTime * 2f);
+  }
 
-                    case '/':
-                        yield return WordGap();
-                        break;
+  IEnumerator WordGap()
+  {
+    yield return new WaitForSeconds(unitTime * 4f);
+  }
 
-                    default:
-                        break;
-                }
-            }
-        }
-    }
+  void EmissionOn()
+  {
+    material.EnableKeyword("_EMISSION");
+    material.SetColor("_EmissionColor", Color.white * bloomMultiplier);
+  }
 
-    IEnumerator Dot()
-    {
-        EmissionOn();
-        yield return new WaitForSeconds(unitTime);
-        EmissionOff();
-        yield return new WaitForSeconds(unitTime);
-    }
-
-    IEnumerator Dash()
-    {
-        EmissionOn();
-        yield return new WaitForSeconds(unitTime * 3f);
-        EmissionOff();
-        yield return new WaitForSeconds(unitTime);
-    }
-
-    IEnumerator LetterGap()
-    {
-        yield return new WaitForSeconds(unitTime * 2f);
-    }
-
-    IEnumerator WordGap()
-    {
-        yield return new WaitForSeconds(unitTime * 4f);
-    }
-
-    void EmissionOn()
-    {
-        material.EnableKeyword("_EMISSION");
-        material.SetColor("_EmissionColor", Color.white * bloomMultiplier);
-    }
-
-    void EmissionOff()
-    {
-        material.SetColor("_EmissionColor", Color.black);
-    }
+  void EmissionOff()
+  {
+    material.SetColor("_EmissionColor", Color.black);
+  }
 }
